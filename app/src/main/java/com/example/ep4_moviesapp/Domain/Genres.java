d25076d6be84d1e4093461784da705affa4a2293
benn_2_2 <benn_2_2@hotmail.com>
2023-11-27 12:12:25 -0500

    FINISHED

---
+++ app/src/main/java/com/example/ep4_moviesapp/Domain/Genres.java
@@ -0,0 +1,15 @@
+package com.example.ep4_moviesapp.Domain;
+
+import java.util.List;
+
+public class Genres {
+    private List<GenresItem> genres;
+
+    public List<GenresItem> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GenresItem> genres) {
+        this.genres = genres;
+    }
+}
